ae3fb515833f2364156f83a6d10db8181011817f
JanRuijtenberg <jan.ruijtenberg@hotmail.com>
2021-12-01 15:07:19 +0100

    use string format

--- src/main/java/AdventOfCodeAnswers.java
+++ src/main/java/AdventOfCodeAnswers.java
@@ -4,8 +4,8 @@ public class AdventOfCodeAnswers {
 
 
     public static void main(String[] args) {
-        System.out.println("The answer to the day 1 exercise 1 is: "+ new SonarSweeper().calculateNrOfIncreasedDepths("day1/input.txt"));
-        System.out.println("The answer to the day 1 exercise 2 is: "+ new SonarSweeper().calculateNrOfIncreasedDepthsWithSlidingWindow("day1/input.txt"));
+        System.out.println(String.format("The answer to the day 1 exercise 1 is: %d", new SonarSweeper().calculateNrOfIncreasedDepths("day1/input.txt")));
+        System.out.println(String.format("The answer to the day 1 exercise 2 is: %d", new SonarSweeper().calculateNrOfIncreasedDepthsWithSlidingWindow("day1/input.txt")));
     }
 
 }
