387b5656ec10ff0bef61ab91c80dd9c1bdcdc907
JanRuijtenberg <jan.ruijtenberg@hotmail.com>
2021-12-02 10:13:37 +0100

    solve first puzle calculating position based on input

--- src/main/java/AdventOfCodeAnswers.java
+++ src/main/java/AdventOfCodeAnswers.java
@@ -6,6 +6,7 @@ public class AdventOfCodeAnswers {
     public static void main(String[] args) {
         System.out.println(String.format("The answer to the day 1 exercise 1 is: %d", new SonarSweeper().calculateNrOfIncreasedDepths("day1/input.txt")));
         System.out.println(String.format("The answer to the day 1 exercise 2 is: %d", new SonarSweeper().calculateNrOfIncreasedDepthsWithSlidingWindow("day1/input.txt")));
+        System.out.println(String.format("The answer to the day 2 exercise 2 is: %d", new Navigator().calculatePosition("day2/input.txt")));
     }
 
 }
