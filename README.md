262a3dcfac56f142a39f899c78641579287574dd
JanRuijtenberg <jan.ruijtenberg@hotmail.com>
2021-12-03 20:40:38 +0100

    improve on readability of readme's

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# advent-of-code-2021
+# advent of code 2021
 
-Advent of code is a series of coding puzzles leading up to chrismass, you can find it here
+Advent of code is a series of coding puzzles leading up to Christmas, you can find it here
 https://adventofcode.com/2021
 
-in here you will find all the solutions to the excercises written in java 
+in here you will find all the solutions to the exercises written in java, there is a separate readme in the folder of the day
+explaining the specific task 
 
--- src/main/java/AdventOfCodeAnswers.java
+++ src/main/java/AdventOfCodeAnswers.java
@@ -8,7 +8,7 @@ public class AdventOfCodeAnswers {
     public static void main(String[] args) {
         System.out.println(String.format("The answer to the day 1 exercise 1 is: %d", new SonarSweeper().calculateNrOfIncreasedDepths("day1/input.txt")));
         System.out.println(String.format("The answer to the day 1 exercise 2 is: %d", new SonarSweeper().calculateNrOfIncreasedDepthsWithSlidingWindow("day1/input.txt")));
-        System.out.println(String.format("The answer to the day 2 exercise 2 is: %d", new Navigator().calculatePosition("day2/input.txt")));
+        System.out.println(String.format("The answer to the day 2 exercise 1 is: %d", new Navigator().calculatePosition("day2/input.txt")));
         System.out.println(String.format("The answer to the day 2 exercise 2 is: %d", new Navigator().calculatePositionWithAim("day2/input.txt")));
         System.out.println(String.format("The answer to the day 3 exercise 1 is: %d", new BinaryDiagnoser().getPower("day3/input.txt")));
         System.out.println(String.format("The answer to the day 3 exercise 2 is: %d", new BinaryDiagnoser().getLifeSupport("day3/input.txt")));
